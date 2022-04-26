15232e056f5920e8b78ef5abe03197995e56f53a
Gerson Santos <jhxn07@gmail.com>
2022-04-25 23:35:06 -0500

    contador con funciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,12 @@
 export class AppComponent {
  title:string = 'Contador';
  num:number =10;
+
+ sumar(){
+   this.num +=1;
+ }
+ resta(){
+   this.num -=1;
+  }
+  //  se implementa un método para suma y resta con la expresión para decremento y e incremento 
 }
