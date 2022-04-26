fc75681b07d3c18e4f185f32bdcb8ab0bc8f1808
Gerson Santos <jhxn07@gmail.com>
2022-04-26 16:14:47 -0500

    Creacion de un componente manual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
-
+import {HeroeComponent} from './heroes/heroe/heroe.component'
 @NgModule({
-  declarations: [
+  declarations: [ 
     AppComponent,
-    ContadorComponent
+    ContadorComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
