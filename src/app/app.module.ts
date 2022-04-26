79e1ae7a20673a27ab5ab3e09cf171d5fef68500
Gerson Santos <jhxn07@gmail.com>
2022-04-26 18:01:10 -0500

    creacion del componente listado automatico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
-import {HeroeComponent} from './heroes/heroe/heroe.component'
+import {HeroeComponent} from './heroes/heroe/heroe.component';
+import { ListadoComponent } from './heroes/listado/listado.component'
 @NgModule({
   declarations: [ 
     AppComponent,
     ContadorComponent,
-    HeroeComponent
+    HeroeComponent,
+    ListadoComponent
   ],
   imports: [
     BrowserModule,
