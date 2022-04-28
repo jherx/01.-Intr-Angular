b82a4a893dbcf0c386b97a9ee1af6319c653f953
Gerson Santos <jhxn07@gmail.com>
2022-04-28 16:18:52 -0500

    Creacion de modulo contador de manera manual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
+// MODULOS CREADOS MANUALMENTE
 import { HeroesModule } from "./heroes/heroes.module";
+import { contadorModule } from './contador/contador.module';
 
 
 @NgModule({
   declarations: [ 
     AppComponent,
-    ContadorComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HeroesModule
+    HeroesModule,
+    contadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
