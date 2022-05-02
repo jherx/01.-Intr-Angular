1e34f25ef8d33e23d0fd1dfbf5050a4d1d1fc3d8
Gerson Santos <jhxn07@gmail.com>
2022-05-02 16:21:43 -0500

    importacion de formmodul para nuestro formulario

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import {NgModule} from '@angular/core';
+import { NgModule} from '@angular/core';
 import { HeroeComponent } from "./heroe/heroe.component";
 import { ListadoComponent } from "./listado/listado.component";
 
